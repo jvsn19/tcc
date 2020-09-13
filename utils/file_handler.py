3b1444069614ff7f7c405b6b7d331a6d717a310e
João Vasconcelos <jvsn@fb.com>
2020-09-13 11:10:05 -0300

    add file handler class this class shold handle all file requests in the system. Also adding a way to create the csv file with all wikipedia references per page

---
+++ utils/file_handler.py
@@ -0,0 +1,30 @@
+import os
+
+class FileHandler:
+    class __FileHandler:
+        def create_dir(self, path: str):
+            os.makedirs(path, exist_ok=True)
+
+        def write_tables_csv(self, name: str, path: str, content):
+            if not os.path.exists(path):
+                self.create_dir(path)
+
+            with open(f'{path}/{name}.csv', 'w+') as csv_file:
+                csv_file.writelines("references\n")
+
+                for line in content:
+                    csv_file.writelines(f'{line}\n')
+
+
+    instance = None
+
+    def _get_instance(self):
+        if self.instance is None:
+            self.instance = self.__FileHandler()
+
+        return self.instance
+
+    @classmethod
+    def write_tables_csv(cls, name: str, path: str, content):
+        instance = cls._get_instance(cls)
+        return instance.write_tables_csv(name, path, content)
