c765bbdc20e9b049d56ecf4e1db36a83800f093c
João Vasconcelos <jvsn19@gmail.com>
2020-09-13 15:54:13 -0300

    remove unused code

--- parsers/TCCParser.py
+++
@@ -1,8 +0,0 @@
-from ..utils.readers import CSVReader
-from ..utils.url_pages import URLPages
-
-class TCCParser:
-    def __init__(self, csv_path: str):
-        self.csv_path: csv_path
-
-    def run():
