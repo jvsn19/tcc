810f4007cdddc8be1db335eb1d2399a1ce93ed75
João Vasconcelos <jvsn19@gmail.com>
2020-09-13 15:50:34 -0300

    finish base crawler

--- utils/crawlers/BaseCrawler.py
+++ utils/crawlers/BaseCrawler.py
@@ -5,20 +5,32 @@ from time import sleep
 from ..logger import Logger
 
 class BaseCrawler(ABC):
-    def __init__(self, root_path: str, parser):
+    '''
+    Basic crawler implementation. A crawler is a bot that, given an url, downloads the html and,
+    with a parser, get all the useful information from the webpage and continues to navigate throught
+    the pages within the current one.
+    The crawler works with a set of urls (set was choosen because it's a good/basic way to avoid repeated url)
+    and a parser. Each url in this set will be downloaded and parsed by our parser. The parser decides what is
+    the desired information. At the end of the parser phase, the parser should return new urls to add to our crawler,
+    so it can move forward to new urls.
+    params:
+    - root_path: the root path from an url. For example: https://www.github.com
+    - parser: the helper parser. The parser function is to, given an url, get all the useful information
+      from it
+    - useragent: the bot crawling the webpage.
+      Default: *
+    '''
+    def __init__(self, root_path: str, parser, useragent = '*'):
         self.root_path = root_path
-        self.useragent = '*'
+        self.useragent = useragent
         self.urls = set()
         self.sleep_time = 5
-        self._parser = parser
+        self.parser = parser
         self._configure_robots_parser()
 
     # Properties
     @property
     def parser(self):
-        if self._parser is None:
-            Logger.log_error("Parser not defined")
-            return None
         return self._parser
 
     @parser.setter
@@ -27,21 +39,35 @@ class BaseCrawler(ABC):
 
     # Private Methods
     def _configure_robots_parser(self):
+        '''
+        Configures the robots.txt. The robots.txt is responsible to avoid accessing forbidden pages
+        '''
         self.robots_parser = robotparser.RobotFileParser(f'{self.root_path}/robots.txt')
         self.robots_parser.read()
 
     def _sleep(self):
+        '''
+        Method responsible for stopping the execution, to avoid overloading the website requests
+        '''
         sleep(self.sleep_time)
 
     # public Methods
     def add_urls(self, urls):
+        '''
+        Method to add new urls to the crawler.
+        '''
         self.urls.update(urls)
 
     def run(self):
+        '''
+        Method to start crawler.
+        '''
         while self.urls:
             url = self.urls.pop()
             self._parser.root_url = url
+
             if not self.robots_parser.can_fetch(self.useragent, url):
+                # robots.txt forbids us to parse this file
                 continue
 
             try:
@@ -52,6 +78,7 @@ class BaseCrawler(ABC):
             except Exception as exception:
                 raise exception
 
+            # Add new urls to crawl
             self.add_urls(new_urls)
 
             if(len(self.urls) > 0):
@@ -59,4 +86,3 @@ class BaseCrawler(ABC):
                 self._sleep()
 
         Logger.log_info(f'Crawler finished.')
-        return 0
